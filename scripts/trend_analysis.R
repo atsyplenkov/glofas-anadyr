39bf2c73c51b01dbb90157f517eb8b7d1142dfe3
atsyplenkov <atsyplenkov@fastmail.com>
2025-12-11 14:20:58 +0300

    trend analysis

---
+++ scripts/trend_analysis.R
@@ -0,0 +1,132 @@
+library(dplyr)
+library(tidyr)
+library(lubridate)
+
+source("src/utils_ggplot.R")
+source("src/utils.R")
+theme_set(theme_mw())
+
+# Read data -----------------------------------------------------------
+obs_data <-
+  fs::dir_ls("data/hydro/obs", regexp = ".csv$") |>
+  purrr::map_dfr(
+    ~ readr::read_csv(.x, show_col_types = FALSE),
+    .id = "gauge_id"
+  ) |>
+  mutate(
+    gauge_id = stringr::str_remove_all(gauge_id, "data/hydro/obs/|.csv"),
+    date = lubridate::as_date(date),
+    q = q_cms,
+    type = "obs",
+    .keep = "unused"
+  ) |>
+  filter(!is.na(q))
+
+cor_data <-
+  fs::dir_ls("data/hydro/cor", regexp = ".csv$") |>
+  purrr::map_dfr(
+    ~ readr::read_csv(.x, show_col_types = FALSE),
+    .id = "gauge_id"
+  ) |>
+  mutate(
+    gauge_id = stringr::str_remove_all(gauge_id, "data/hydro/cor/|.csv"),
+    date = lubridate::as_date(date),
+    q = q_cor,
+    type = "cor",
+    .keep = "unused"
+  )
+
+raw_data <-
+  fs::dir_ls("data/hydro/raw", regexp = ".csv$") |>
+  purrr::map_dfr(
+    ~ readr::read_csv(.x, show_col_types = FALSE),
+    .id = "gauge_id"
+  ) |>
+  mutate(
+    gauge_id = stringr::str_remove_all(gauge_id, "data/hydro/raw/|.csv"),
+    date = lubridate::as_date(datetime) - 1,
+    q = q_raw,
+    type = "raw",
+    .keep = "unused"
+  )
+
+# Merge data -----------------------------------------------------------
+all_data <-
+  bind_rows(obs_data, cor_data, raw_data) |>
+  mutate(year = year(date), month = month(date)) |>
+  mutate(type = factor(type, levels = c("obs", "raw", "cor")))
+
+m_data <-
+  all_data |>
+  filter(between(month, 5, 10)) |>
+  group_by(gauge_id, type, year) |>
+  reframe(q_mean = mean(q), q_max = max(q)) |>
+  mutate(q_mean = ifelse(q_mean < 10, NA_real_, q_mean))
+
+annual_trends <-
+  m_data |>
+  ggplot(aes(x = year, y = q_mean, color = type)) +
+  # geom_abline(
+  #   data = trends,
+  #   aes(slope = slope, intercept = intercept, color = type)
+  # ) +
+  geom_smooth(se = FALSE, method = "lm") +
+  geom_line() +
+  scale_color_manual(
+    name = "Streamflow",
+    values = c(mw_black, mw_red, mw_blue),
+    labels = c("Observed", "Raw GloFAS-ERA5", "Bias-corrected GloFAS-ERA5")
+  ) +
+  labs(
+    x = "",
+    y = "Mean annual water discharge, m³/s"
+  ) +
+  facet_wrap(~gauge_id, scales = "free") +
+  theme(
+    legend.position = "inside",
+    # legend.direction = "horizontal",
+    legend.position.inside = c(0.35, 0.15)
+  )
+
+save_png(
+  "figures/fig06_annual-trends.png",
+  annual_trends,
+  dpi = 300,
+  w = 18,
+  h = 12
+)
+
+# Estimate trends -----------------------------------------------------------
+library(purrr)
+
+trends <-
+  m_data |>
+  group_by(gauge_id, type) |>
+  mutate(period = glue::glue("{min(year)}–{max(year)}")) |>
+  group_by(gauge_id, period, type) |>
+  nest() |>
+  mutate(trend = map_dfr(data, ~ mw_kendall(.x$year, .x$q_mean))) |>
+  unnest(trend) |>
+  select(-data) |>
+  ungroup()
+
+trends |>
+  select(-slope:-tau) |>
+  relocate(gauge_id:period, pct, p) |>
+  mutate(
+    type = forcats::fct_recode(
+      type,
+      "Observed" = "obs",
+      "Raw GloFAS-ERA5" = "raw",
+      "Bias-corrected GloFAS-ERA5" = "cor"
+    )
+  ) |>
+  mutate(across(where(is.numeric), ~ mw_round(.x))) |>
+  write.csv(
+    "tables/tbl3_trends-results.csv",
+    quote = FALSE,
+    na = "",
+    row.names = FALSE
+  )
+
+
--- src/utils.R
+++ src/utils.R
@@ -27,8 +27,8 @@ mw_round <- function(x) {
     xa < 10,
     ifelse(
       xa < 1,
-      # format(x, digits = 2, scientific = FALSE),
-      round(x, 2),
+      format(x, digits = 2, scientific = FALSE),
+      # round(x, 2),
       round(x, 2)
     ),
     ifelse(xa < 100, round(x, 1), round(x, 0))
@@ -50,3 +50,47 @@ mw_round <- function(x) {
 
   trimws(result)
 }
+# Mann-Kendall test ------------------------------------------------------
+# Modified from
+# https://github.com/USGS-R/EGRETextra/blob/fb1b44a1107dbba1af3f179466814f488e8a5627/vignettes/vignetteFlowWeighted.Rmd#L74-L81
+
+mw_kendall <-
+  function(.date, .y, .remove = NULL) {
+    # Early return if there is insufficient amount of datapoints
+    # see help("rkt", "rkt")
+    if (any(length(.y) < 4, length(.date) < 4)) {
+      return(
+        c(
+          "slope" = NA_real_,
+          "intercept" = NA_real_,
+          "tau" = NA_real_,
+          "p" = NA_real_,
+          "pct" = NA_real_
+        )
+      )
+    }
+
+    # Replace values of interest with NAs
+    if (!is.null(.remove)) {
+      .y[.remove] <- NA_real_
+    }
+
+    # Estimate Mann-Kendall trend
+    mk_test <- rkt::rkt(date = .date, y = .y)
+    mk_slope <- mk_test$B # Theil-Sen slope
+    mk_intercept <- median(.y, na.rm = TRUE) -
+      mk_slope * median(.date)
+
+    # Percent change
+    # fmt:skip
+    mk_pct <- 100 * mk_slope / exp(mean(log(.y), na.rm = TRUE))
+
+    # Return
+    c(
+      "slope" = mk_slope,
+      "intercept" = mk_intercept,
+      "tau" = mk_test$tau,
+      "p" = mk_test$sl,
+      "pct" = mk_pct
+    )
+  }
